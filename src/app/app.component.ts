28f3e0cf75c45b5297547a27ad9ef5c60b5389c9
koto007 <kotomiura@icloud.com>
2021-01-20 16:08:39 +0100

    Add todo list array and use ngFor to display them in template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+
+  public todoArray: Array<string> = [
+    'révision angular',
+    'cuisine',
+    'promenade' 
+  ];
+
 }
