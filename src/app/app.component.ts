ade520128f37ead765495e47fc86dff7d1a7272d
koto007 <kotomiura@icloud.com>
2021-01-21 13:32:46 +0100

    Add addItem listner with a better solution to handle HTMLInputElement and add removing element from todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,15 +16,39 @@ export class AppComponent {
 
   public addItem($event: KeyboardEvent): void{
     if($event.code === "Enter"){
-      const $input: HTMLInputElement|null = $event.target as HTMLInputElement;
-
-      if($input !== null){
-        console.log($input.value);
+      //const $input: HTMLInputElement|null = $event.target as HTMLInputElement;
+      
+      const $input: EventTarget|null = $event.target;
+      console.log("input", $input);
+      if($input instanceof HTMLInputElement){
+        const str = $input.value.trim();
+        if(str !== ""){
+          this.todoArray.push($input.value);
+          $input.value ="";
+  
+          console.log($input.value);
+        }
+        
       }
+
+      /*if($input !== null){
+        console.log($input.value);
+      }*/
 
       console.log("enter pressed");
 
+      
+
+
 
     };
+  }
+  public removeItem(index: number): void {
+
+      if(index !== -1){
+        this.todoArray.splice(index, 1);
+      }
+          
+    console.log(this.todoArray);
   }
 }
